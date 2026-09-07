b5e0f0fc0b25c135f538ae80afcc67524016cd3a
Uddalak <84083101+Imdavid21@users.noreply.github.com>
2026-09-07 18:28:04 +0530

    feat: export 1delta integration module

---
+++ apps/web/src/features/Swap/oneDelta/index.ts
@@ -0,0 +1,12 @@
+export { OneDeltaClient } from './client'
+export { selectExecutionPlan } from './execution'
+export { OneDeltaSwapProvider } from './provider'
+export type { SwapProvider } from './provider'
+export type {
+  OneDeltaActions,
+  OneDeltaSpotSwapRequest,
+  OneDeltaSpotSwapResponse,
+  OneDeltaTransaction,
+  TokosSwapExecution,
+  TokosSwapQuote,
+} from './types'
